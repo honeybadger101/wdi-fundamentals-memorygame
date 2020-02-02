eece014438a2304441fc71fc8a1414f8df0e9222
honeybadger101 <60223606+honeybadger101@users.noreply.github.com>
2020-02-02 09:19:39 +0000

    3

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,3 @@
-console.log("Up and running!");
 
 const cards = ["queen", "queen", "king", "king"]
 
@@ -6,3 +5,22 @@ var cardsInPlay = [];
 
 var cardOne = cards[0];
 cardsInPlay.push(cards[0]);
+console.log('User filpped queen');
+
+var cardTwo = cards[1];
+cardsInPlay.push(cards[1]);
+console.log('User flipped king');
+
+var cardThree = cards[2];
+cardsInPlay.push(cards[2]);
+console.log('User flipped king');
+
+var cardFour = cards[3];
+cardsInPlay.push(cards[3]);
+console.log('User filpped king');
+
+if (cardsInPlay[0]) === cardsInPlay[1] {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again");	
+}	
