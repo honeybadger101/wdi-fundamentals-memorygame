7a298cdda19bcc37655a04298278554a197d4f4a
honeybadger101 <60223606+honeybadger101@users.noreply.github.com>
2020-02-02 09:31:18 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,26 +1,21 @@
+console.log("Up and running!");
 
-const cards = ["queen", "queen", "king", "king"]
-
+var cards = ["queen", "queen", "king", "king"]
 var cardsInPlay = [];
 
 var cardOne = cards[0];
-cardsInPlay.push(cards[0]);
-console.log('User filpped queen');
+var cardTwo = cards[2];
 
-var cardTwo = cards[1];
-cardsInPlay.push(cards[1]);
-console.log('User flipped king');
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
-var cardThree = cards[2];
-cardsInPlay.push(cards[2]);
-console.log('User flipped king');
+console.log("User filpped" + cardOne);
+console.log("User flipped" + cardTwo);
 
-var cardFour = cards[3];
-cardsInPlay.push(cards[3]);
-console.log('User filpped king');
-
-if (cardsInPlay[0]) === cardsInPlay[1] {
+if (cardsInPlay.length === 2) {
+if (cardsInPlay[0] === cardsInPlay[1]) { 	
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again");	
+}
 }	
